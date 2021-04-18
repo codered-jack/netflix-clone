f8a50a3dee13f1fd3c8a9ed3ec4b93d4e082dc06
Sumit Kumar <sumit.kumar@pinelabs.com>
2021-04-18 23:21:38 +0530

    netflix clone

---
+++ src/Requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "ad7fd682ba0e1476004abc8869f1d01d";
+
+const requests = {
+    fetchTrending:`/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals:`/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies:`/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies:`/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies:`/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies:`/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries:`/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBwiI81Q3uUlytvX8S3DQDjftr2YBu2jjM",
+    authDomain: "netflix-clone-c7131.firebaseapp.com",
+    projectId: "netflix-clone-c7131",
+    storageBucket: "netflix-clone-c7131.appspot.com",
+    messagingSenderId: "766722231104",
+    appId: "1:766722231104:web:78f31292d0fe3219cb88a5"
+  };
